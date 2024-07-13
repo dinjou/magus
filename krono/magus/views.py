f1643b27c7e8186d6749a9cd226bf2d337add160
slay <slay@smartaira.com>
2024-07-13 17:23:46 -0600

    Fix User Registration Form error

--- krono/magus/views.py
+++ krono/magus/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+
+from .forms import UserRegisterForm
 from .models import Task, Profile
 from django.utils import timezone
 from django.contrib import messages
