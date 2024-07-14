02e2450f4b6e6661977f383f3387e15a618c5591
malathair <malathair@spectregate.net>
2024-07-14 12:43:12 -0600

    Add admin UI support for tasks

--- krono/magus/admin.py
+++ krono/magus/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "task_type",
+        "start_time",
+        "end_time",
+        "interrupted",
+    ]
+    list_per_page = 25
+    readonly_fields = [
+        "user",
+        "task_type",
+    ]
